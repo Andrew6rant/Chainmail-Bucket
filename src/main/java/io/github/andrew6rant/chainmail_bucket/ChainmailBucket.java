aa181f03e03efb8e07e3d0d6ed7e0a882c3da3b0
acikek <spiralixp@gmail.com>
2023-02-20 10:25:44 -0800

    xd

--- src/main/java/io/github/andrew6rant/chainmail_bucket/ChainmailBucket.java
+++ src/main/java/io/github/andrew6rant/chainmail_bucket/ChainmailBucket.java
@@ -1,22 +1,19 @@
 package io.github.andrew6rant.chainmail_bucket;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class ChainmailBucket implements ModInitializer {
     public static final ArmorMaterial CUSTOM_CHAINMAIL_ARMOR_MATERIAL = new ChainmailBucketArmorMaterial();
 
-    public static final ChainmailBucketItem CHAINMAIL_BUCKET = new ChainmailBucketItem(CUSTOM_CHAINMAIL_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings());
+    public static final ChainmailBucketItem CHAINMAIL_BUCKET = new ChainmailBucketItem(CUSTOM_CHAINMAIL_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.MISC));
 
 
     @Override
     public void onInitialize() {
-        Registry.register(Registries.ITEM, new Identifier("chainmail_bucket", "chainmail_bucket"), CHAINMAIL_BUCKET);
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> entries.addAfter(Items.BUCKET, CHAINMAIL_BUCKET));
+        Registry.register(Registry.ITEM, new Identifier("chainmail_bucket", "chainmail_bucket"), CHAINMAIL_BUCKET);
     }
 }
